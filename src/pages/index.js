135c56c3e9139065d0470141c6e447295a7e47d9
zak <codedpills@gmail.com>
2021-03-21 14:29:50 +0000

    render Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import MainNavbar from '../components/Navbar/MainNavbar';
+
 const Index = () => {
     return (
         <div>
+            <MainNavbar />
             <h1>Welcome to Concert Party!</h1>
         </div>
     );
