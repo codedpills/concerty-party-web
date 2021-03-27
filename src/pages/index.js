c6231e4f355a51c1f139eb6729277918aa6c529d
zak <codedpills@gmail.com>
2021-03-27 12:31:23 +0000

    render EmailListFormDisplay

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import HomeHero from '../components/Hero/HomeHero';
+import EmailListFormDisplay from '../components/EmailListForm/EmailListFormDisplay';
 
 const Index = () => {
     return (
         <Layout>
             <HomeHero />
+            <EmailListFormDisplay />
         </Layout>
     );
 }
