7a49f109aae0ecf0898c60b2fa2aa631966e0b8a
zak <codedpills@gmail.com>
2021-04-07 17:57:49 +0000

    render title from markdown frontmatter

--- src/components/About/WhyConcertParty.jsx
+++ src/components/About/WhyConcertParty.jsx
@@ -1,11 +1,24 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 import img from "../../images/hero/anthony-delanoix-hzgs56Ze49s-unsplash.jpg"
 
 const WhyConcertParty = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      markdownRemark {
+        frontmatter {
+          title
+        }
+      }
+    }
+  `)
+
+  console.log(data)
+
   return (
     <div className="why-concert-party">
-      <h4>Why Concert Party</h4>
+      <h4>{data.markdownRemark.frontmatter.title}</h4>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nemo
         voluptas officia pariatur molestiae iusto est dolores consequatur
