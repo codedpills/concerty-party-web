a3afe585977d0ea7024b100364b9c747b29042d8
zak <codedpills@gmail.com>
2021-04-07 15:01:54 +0000

    render About

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Layout from '../components/Layout';
 import HomeHero from '../components/Hero/HomeHero';
 import EmailListFormDisplay from '../components/EmailListForm/EmailListFormDisplay';
+import About from '../components/About/About';
 
 const Index = () => {
     return (
         <Layout>
             <HomeHero />
             <EmailListFormDisplay />
+            <About />
         </Layout>
     );
 }
