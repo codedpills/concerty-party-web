17251c524e45d0265887c1855923549c511d4c24
zak <codedpills@gmail.com>
2021-04-07 15:01:44 +0000

    add template for whyconcertparty

---
+++ src/components/About/WhyConcertParty.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+import img from "../../images/hero/anthony-delanoix-hzgs56Ze49s-unsplash.jpg"
+
+const WhyConcertParty = () => {
+  return (
+    <div className="why-concert-party">
+      <h4>Why Concert Party</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nemo
+        voluptas officia pariatur molestiae iusto est dolores consequatur
+        expedita eveniet porro saepe, consectetur libero nobis assumenda facilis
+        magnam dicta natus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nemo
+        voluptas officia pariatur molestiae iusto est dolores consequatur
+        expedita eveniet porro saepe, consectetur libero nobis assumenda facilis
+        magnam dicta natus!
+      </p>
+      <img src={img} alt="img" className="img-fluid" />
+    </div>
+  )
+}
+
+export default WhyConcertParty
