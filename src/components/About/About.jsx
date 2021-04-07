30349d91b0af113cb52a9f77aaf91b35718f6b81
zak <codedpills@gmail.com>
2021-04-07 15:10:07 +0000

    update text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+import "./About.scss"
 import WhyConcertParty from "./WhyConcertParty"
 
 const About = () => {
@@ -11,6 +12,7 @@ const About = () => {
             <WhyConcertParty />
           </div>
           <div className="col-4">
+            <h4>Find Us</h4>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
             voluptatibus beatae praesentium nostrum odit consequuntur quasi,
             facere, soluta ipsa error dolorem earum. Laboriosam, incidunt quae
