1cb49189320883fcefc10657ff4e2974a12de9ea
zak <codedpills@gmail.com>
2021-04-11 21:11:45 +0000

    set up concerts page

---
+++ src/pages/concerts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import HomeHero from '../components/Hero/HomeHero';
+
+const Concerts = () => {
+    return (
+        <Layout>
+            <HomeHero />
+        </Layout>
+    );
+}
+
+export default Concerts;
