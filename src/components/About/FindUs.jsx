bd76801ced4aab7a34f296e2323b1e45a014999a
zak <codedpills@gmail.com>
2021-04-11 12:01:59 +0000

    add google map

--- src/components/About/FindUs.jsx
+++ src/components/About/FindUs.jsx
@@ -7,16 +7,24 @@ const FindUs = () => {
       <h4>Find Us</h4>
       <div className="location">
         <h5>Location</h5>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.7734819098005!2d-0.17955748523388076!3d5.600446095939848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9b24d499ad2d%3A0x8354c5d0aa1d3b04!2sOne%20Airport%20Square!5e0!3m2!1sen!2sgh!4v1618142134026!5m2!1sen!2sgh"
+          width=""
+          height=""
+          style={{"border": 0, "marginTop": "0.5rem"}}
+          allowfullscreen=""
+          loading="lazy"
+        ></iframe>
         <ul>
-            <li>One Airport Square</li>
-            <li>Accra, Ghana</li>
+          <li>One Airport Square</li>
+          <li>Accra, Ghana</li>
         </ul>
       </div>
       <div className="phone">
         <h5>Phone</h5>
         <ul>
-            <li>+233 409 3443 3414</li>
-            <li>+233 409 3443 3412</li>
+          <li>+233 409 3443 3414</li>
+          <li>+233 409 3443 3412</li>
         </ul>
       </div>
       <div className="socials">
