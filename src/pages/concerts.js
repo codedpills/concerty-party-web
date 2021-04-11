389778517b65186e2e671b193cf0d8835f3f4cd7
zak <codedpills@gmail.com>
2021-04-11 22:19:25 +0000

    add markup to render Concert

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import HomeHero from '../components/Hero/HomeHero';
+import Concert from '../components/Concerts/Concert';
 
 const Concerts = () => {
     return (
         <Layout>
             <HomeHero />
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Concert />
+                    </div>
+                    <div className="col"></div>
+                    <div className="col"></div>
+                </div>
+            </div>
         </Layout>
     );
 }
