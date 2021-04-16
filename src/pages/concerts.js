30e320da097e1f82ffbf240438e2700ebbe6775a
zak <codedpills@gmail.com>
2021-04-16 13:00:36 +0000

    remove extra markup

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -2,21 +2,13 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import HomeHero from '../components/Hero/HomeHero';
-import Concert from '../components/Concerts/Concert';
+import FeaturedConcerts from '../components/Concerts/FeaturedConcerts';
 
 const Concerts = () => {
     return (
         <Layout>
             <HomeHero />
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <Concert />
-                    </div>
-                    <div className="col"></div>
-                    <div className="col"></div>
-                </div>
-            </div>
+            <FeaturedConcerts />
         </Layout>
     );
 }
