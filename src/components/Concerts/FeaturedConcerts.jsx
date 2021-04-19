1c9d48049aebb12f5f6dbede8153b54dfbeecdc9
zak <codedpills@gmail.com>
2021-04-19 23:12:28 +0000

    render concerts from Contentful

--- src/components/Concerts/FeaturedConcerts.jsx
+++ src/components/Concerts/FeaturedConcerts.jsx
@@ -1,18 +1,57 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
-import Concert from './Concert';
+import Concert from "./Concert"
 
 const FeaturedConcerts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulFeaturedconcerts {
+        edges {
+          node {
+            id
+            name
+            day
+            month
+            slug
+            description {
+              description
+            }
+            image {
+              file {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const concerts = data.allContentfulFeaturedconcerts.edges;
+
+  const featuredConcerts = concerts.map(concert => {
+    const details = {
+      name: concert.node.name,
+      description: concert.node.description.description,
+      month: concert.node.month,
+      day: concert.node.day,
+      slug: concert.node.slug,
+      image: concert.node.image.file.url,
+    }
+    return (
+      <div className="col-md-4" key={concert.node.id}>
+        <Concert concertDetails={details} />
+      </div>
+    )
+  })
+
   return (
     <div className="featured-concerts">
       <div className="container">
-        <div className="row">
-          <div className="col">
-            <Concert />
-          </div>
-          <div className="col"></div>
-          <div className="col"></div>
-        </div>
+        <h4>Featured Concerts</h4>
+        <br/>
+        <div className="row">{featuredConcerts}</div>
       </div>
     </div>
   )
