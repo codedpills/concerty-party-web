1bb3b16066d90a7acdbfc44bd083a9e2ffbc461b
zak <codedpills@gmail.com>
2021-04-22 15:07:08 +0000

    dynamically create pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const concertDetailsTemplate = path.resolve(
+    `./src/templates/ConcertDetails.jsx`
+  )
+
+  // Query data from Contentful
+  const result = await graphql(`
+    query {
+      allContentfulFeaturedconcerts {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  console.log("result", result)
+
+  // create pages for each concert
+  const concerts = result.data.allContentfulFeaturedconcerts.edges
+  concerts.map(({ node }) => {
+    node.url = `/concerts/${node.slug}`
+
+    actions.createPage({
+      path: node.url,
+      component: concertDetailsTemplate,
+      context: { slug: node.slug },
+    })
+  })
+}
