a5c4fc859480d66a8d83d4c6d0e01fa43259651e
zak <codedpills@gmail.com>
2021-04-23 12:38:42 +0000

    add LoginForm

---
+++ src/components/Login/LoginForm.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+const LoginForm = () => {
+  const handleLogin = e => {
+    e.preventDefault()
+    console.log("Login successful")
+    e.target
+  }
+
+  return (
+    <form>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          aria-describedby="emailHelp"
+        />
+        <small id="emailHelp" class="form-text text-muted">
+          We'll never share your email with anyone else.
+        </small>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" />
+      </div>
+      <button type="submit" class="btn btn-dark" onClick={handleLogin}>
+        Login
+      </button>
+    </form>
+  )
+}
+
+export default LoginForm
