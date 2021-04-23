8d4ed1204ff070572e98bee04a52b270d2eb943f
zak <codedpills@gmail.com>
2021-04-23 12:18:01 +0000

    create login page

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginWrapper from '../components/Login/LoginWrapper';
+
+const Login = () => {
+    return (
+        <div>
+            <LoginWrapper />
+        </div>
+    );
+}
+
+export default Login;
