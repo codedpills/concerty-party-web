cf52460d2732c42a0297bfe5907cc38cda986f7f
zak <codedpills@gmail.com>
2021-04-24 09:08:22 +0000

    add background image

--- src/components/Login/LoginWrapper.jsx
+++ src/components/Login/LoginWrapper.jsx
@@ -1,14 +1,50 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
 import "./login.scss"
 
 import LoginForm from "./LoginForm"
 
 const LoginWrapper = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        desktop: file(
+          relativePath: {
+            eq: "images/hero/jordon-conner-tIr-PWgSYB4-unsplash.jpg"
+          }
+        ) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const bgImg = data.desktop.childImageSharp.fluid
+
   return (
     <div className="login-wrapper">
       <div className="row no-gutters">
-        <div className="col-5">bg image</div>
+        <div className="col-5">
+          <div className="background-section">
+            <BackgroundImage
+              fluid={bgImg}
+              backgroundColor={`#040e18`}
+              style={{ height: "100vh" }}
+            >
+              <div className="bg-text">
+                <h1>When you want to live, you go to concerts!</h1>
+                <br />
+                <p>Photo by Jordon Conner on Unsplash</p>
+              </div>
+            </BackgroundImage>
+          </div>
+        </div>
         <div className="col">
           <div className="login-form-section">
             <div className="logo-area">
