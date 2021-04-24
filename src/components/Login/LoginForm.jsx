783d1043bd27ecff32da07307cc2e7743508c7ae
zak <codedpills@gmail.com>
2021-04-24 09:07:53 +0000

    change class to className

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -8,23 +8,23 @@ const LoginForm = () => {
 
   return (
     <form>
-      <div class="form-group">
+      <div className="form-group">
         <label for="email">Email</label>
         <input
           type="email"
-          class="form-control"
+          className="form-control"
           id="email"
           aria-describedby="emailHelp"
         />
-        <small id="emailHelp" class="form-text text-muted">
+        <small id="emailHelp" className="form-text text-muted">
           We'll never share your email with anyone else.
         </small>
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" />
+        <input type="password" className="form-control" id="password" />
       </div>
-      <button type="submit" class="btn btn-dark" onClick={handleLogin}>
+      <button type="submit" className="btn btn-dark" onClick={handleLogin}>
         Login
       </button>
     </form>
