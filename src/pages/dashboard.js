1cf050bd69518d3049e623b90fcd29b047234766
zak <codedpills@gmail.com>
2021-04-26 20:32:53 +0000

    render Profile inside dashboard layout

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react"
+
+import Layout from "../components/Layout"
+import Profile from "../components/Profile/Profile"
 
 const dashboard = () => {
-    return (
-        <div>
-            <h1>My Dashboard</h1>
+  return (
+    <Layout>
+      <div className="container mt-4 mb-4">
+        <h5 className="text-center">MY CONCERT CENTER</h5>
+        <br />
+        <div className="row">
+          <div className="col-3">
+            <Profile />
+          </div>
+          <div className="col">my concerts</div>
         </div>
-    );
+      </div>
+    </Layout>
+  )
 }
 
-export default dashboard;
+export default dashboard
