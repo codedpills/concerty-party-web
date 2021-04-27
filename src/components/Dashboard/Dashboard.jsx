1b715963b805afe3ec282e89b3e22b0bc8bfe5ed
zak <codedpills@gmail.com>
2021-04-27 21:17:24 +0000

    handle errors, add loading state

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -9,6 +9,7 @@ import ConcertCard from "./ConcertCard"
 
 const Dashboard = () => {
   const [concerts, setConcerts] = useState([])
+  const [requesting, setRequesting] = useState(true)
   useEffect(() => {
     axios({
       method: "get",
@@ -21,10 +22,34 @@ const Dashboard = () => {
       .then(res => {
         console.log(res)
         setConcerts(res.data.results)
+        setRequesting(false)
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err)
+        setRequesting(false)
+      })
   }, [])
   console.log("state", concerts)
+
+  let myConcerts
+  if (requesting === false && concerts.length > 1) {
+    myConcerts = concerts.map(concert => {
+      const venue =
+        concert.entities.length !== 0 ? concert.entities[0].name : "Venue N/A"
+      const concertInfo = {
+        title: concert.title,
+        venue: venue,
+        startDate: concert.start,
+        endDate: concert.end,
+        tags: concert.labels,
+      }
+      return <ConcertCard key={concert.id} concertInfo={concertInfo} />
+    })
+  } else if (requesting === false && concerts.length === 0) {
+    myConcerts = <p>Nothing to see.</p>
+  } else {
+    myConcerts = <p>Loading...</p>
+  }
 
   return (
     <div className="dashboard shadow p-3 mb-5 bg-white rounded">
@@ -34,10 +59,7 @@ const Dashboard = () => {
         </div>
         <Tabs defaultActiveKey="upcoming">
           <Tab eventKey="upcoming" title="Upcoming">
-            <div className="upcoming">
-              <ConcertCard />
-              <ConcertCard />
-            </div>
+            <div className="upcoming">{myConcerts}</div>
           </Tab>
           <Tab eventKey="booked" title="Booked">
             <div className="booked">You have no booked concerts yet.</div>
