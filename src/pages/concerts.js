3283dde84f76249c614234e51be7b9c15a6289fb
zak <codedpills@gmail.com>
2021-05-26 14:42:04 +0000

    add SEO

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react"
+import { Title, Meta } from "react-head"
 
-import Layout from '../components/Layout';
-import HomeHero from '../components/Hero/HomeHero';
-import FeaturedConcerts from '../components/Concerts/FeaturedConcerts';
+import Layout from "../components/Layout"
+import HomeHero from "../components/Hero/HomeHero"
+import FeaturedConcerts from "../components/Concerts/FeaturedConcerts"
 
 const Concerts = () => {
-    return (
-        <Layout>
-            <HomeHero />
-            <FeaturedConcerts />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Title>Featured Concerts | ConcertParty</Title>
+      <Meta
+        name="description"
+        content="Explore and experience the greatest music on earth, and share our discoveries with you.."
+      />
+      <HomeHero />
+      <FeaturedConcerts />
+    </Layout>
+  )
 }
 
-export default Concerts;
+export default Concerts
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react"
+import { Title, Meta } from "react-head"
 
-import Layout from '../components/Layout';
-import HomeHero from '../components/Hero/HomeHero';
-import EmailListFormDisplay from '../components/EmailListForm/EmailListFormDisplay';
-import About from '../components/About/About';
+import Layout from "../components/Layout"
+import HomeHero from "../components/Hero/HomeHero"
+import EmailListFormDisplay from "../components/EmailListForm/EmailListFormDisplay"
+import About from "../components/About/About"
 
 const Index = () => {
-    return (
-        <Layout>
-            <HomeHero />
-            <EmailListFormDisplay />
-            <About />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Title>Welcome to ConcertParty</Title>
+      <Meta
+        name="description"
+        content="Concert Party is a global community connecting music and show lovers to the most amazing
+concerts around the world."
+      />
+      <HomeHero />
+      <EmailListFormDisplay />
+      <About />
+    </Layout>
+  )
 }
 
-export default Index;
+export default Index
